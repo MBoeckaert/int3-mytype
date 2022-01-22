b0edb62e1dc2893b525c936fe2091cbe880f10df
Matthias <matthiasboeckaert@gmail.com>
2022-01-22 12:23:12 +0100

    adding html content & characteristic + nft-why grid css

--- src/view/pages/index.php
+++ src/view/pages/index.php
@@ -1,3 +1,73 @@
-<section class="content">
-<p>wake up mr. west</p>
+<div class="container">
+<!-- landing page -->
+<main>
+  <h1>Clarendon <br>Typeface</h1>
+  <p>If Clarendon was invented today, it would
+probably be released as an NFT. A digital
+object ciruclating in the digital wild west.
+Brought to you by Slanted Publishers.</p>
+</main>
+<!-- section abt Besley -->
+<section class="section__creator">
+<h2>Robert Besley</h2>
+<p>creator of</p>
+<h2>Clarendon</h2>
+<p>
+  Clarendon was published in 1845 after Robert Besley, an employee
+of the fans street foundry since 1826, was made a partner in the
+Firm. Due to its popularity, Besley registered the typeface under
+Britain’s Ornamental Designs Act of 1842. The patent expired
+Three years later and other foundries were quick to copy it.
+Clarendon is considered the first registered typeface.
+</p>
+
+<p>a b c d e f g h i j k l m n o p q r s t u v w x y z a b c d e f g h I j k l m n o p q r s t u v  w x y z 1 2 3 4 5 6 7 8 9 0</p>
 </section>
+<!-- section abt characteristics -->
+<section class="section__characteristics">
+  <div class="characteristics__grid">
+    <div class="bold">bold</div>
+    <div class="display">display</div>
+    <div class="face">face</div>
+    <div class="with">with a</div>
+    <div class="softer">side</div>
+    <div class="side">side</div>
+    <div class="for">for</div>
+    <div class="advertising">advertising</div>
+    <div class="announcements">announcements</div>
+    <div class="displays">display</div>
+  </div>
+
+  <h2>characteristics</h2>
+  <p>Clarendon is an English Slab-Serif. The strokes are fairly heavy of thick and thin weight contrast rather than a unfiform stroke. Characters such as f, a, e, and g have a rounded serif, in which is based on the roman model unlike the rest which is based on the Egyptian bold style. However, the squared serif are not as thick as most of the Egyptian style but rather have subtle brackets. Because of the bracketed serifs and the contrast in weights, it becomes a "problem as stroke become thinner it limits how tight the typeface can go (Velazquez).
+  </p>
+</section>
+<!-- what is NFT section -->
+<section class="section__nft">
+  <p>if clarendon was released today, it would be an</p>
+  <h3>nft?!</h3>
+  <p>NFT stands for Non-Fungible Token. It is a digital art piece or a digital version of a physical artwork and is uploaded on the blockchain, a big excel file on the internet. It certifies a digital asset is therefore not interchangeable, it also gives image rights and ownership back to the creators.
+  </p>
+</section>
+
+<section class="section__why">
+  <div class="why__grid">
+    <div class="why">why</div>
+    <div class="clarendon">clarendon</div>
+    <div class="as-nft">as nft?</div>
+  </div>
+
+  <div class="why__reasons">
+    <p>The Clarendon design was immediately after creation
+    very  popular and rapidly copied by other foundries.
+    Thanks to the blockain, the original can be easily found.</p>
+    <p>Best known as the font of the lawless Wild West era. The
+    crypto landscape is still in it's infancy and governments
+    are still figuring out how they can regulate this space.</p>
+    <p>Robberies & the gold rush were also characteristic for the
+    Wild West. The same is happening with NFT's where big
+    money goes around & many scammers are lurking around.</p>
+  </div>
+</section>
+
+</div>
