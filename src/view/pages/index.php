2bdf85d8bd76eea40adceac7d01f916548fdffbd
Matthias <matthiasboeckaert@gmail.com>
2022-01-26 16:17:25 +0100

    full responsive up to why as nft?

--- src/view/pages/index.php
+++ src/view/pages/index.php
@@ -17,10 +17,9 @@
 <!-- <img class="img__billboard" src="../assets/img/clarendonDesktop.png" alt="clarendon billboard" > -->
   <div class="landing__text-bg">
     <div class="landing-text">
-      <p class="landing__text-content">If Clarendon was invented today, it would
+      <p class="landing__text-content">This week in Slanted mini typography series: Clarendon. If Clarendon was invented today, it would
         probably be released as an NFT. A digital
-        object ciruclating in the digital wild west.
-        Brought to you by Slanted Publishers.</p>
+        object circulating in the digital wild west.</p>
       <div class="read-more">
         <p>read more</p>
         <i class="fas fa-chevron-down"></i>
@@ -37,13 +36,13 @@
   <div class="creator__title">
     <h1>Robert Besley</h1>
     <p>creator of</p>
-    <h1 class="title__clarendon">Clarendon</h1>
+    <h2 class="title__clarendon">Clarendon</h2>
   </div>
   <p class="creator__content">Clarendon was published in 1845 after Robert Besley, an employee
   of the fans street foundry since 1826, was made a partner in the
   Firm. Due to its popularity, Besley registered the typeface under
   Britain’s Ornamental Designs Act of 1842. The patent expired
-  Three years later and other foundries were quick to copy it.
+  three years later and other foundries were quick to copy it.
   Clarendon is considered
   </p>
   <p class="creator__registered uppercase">the first <br>registered typeface.</p>
@@ -70,7 +69,9 @@
 
   <div class="characteristics__text">
     <h2 class="characteristics__title">The first type<br> designed to be bold</h2>
-    <p>Clarendon is an English Slab-Serif. The strokes are fairly heavy of thick and thin weight contrast rather than a unfiform stroke. Characters such as f, a, e, and g have a <span class="orange">rounded serif</span>, in which is based on the roman model unlike the rest which is based on the Egyptian bold style. However, the squared serif are not as thick as most of the Egyptian style but rather have <span class="orange">subtle brackets</span>. Because of the bracketed serifs and the contrast in weights, it becomes a "problem as stroke become thinner it limits how tight the typeface can go (Velazquez).</p>
+    <p>Clarendon is an English Slab-Serif. The strokes are fairly heavy of thick and thin weight contrast rather than a unfiform stroke. Characters such as f, a, e and g have a <span class="orange">rounded serif</span>, in which is based on the roman model unlike the rest which is based on the Egyptian bold style.</p>
+    <p class="characteristics__img">img</p>
+    <p> However, the squared serif are not as thick as most of the Egyptian style but rather have <span class="orange">subtle brackets</span>. Because of the bracketed serifs and the contrast in weights, it becomes a "problem as stroke become thinner it limits how tight the typeface can go (Velazquez).</p>
   </div>
 </section>
 
@@ -84,13 +85,17 @@
     <p class="nft-tab2">  it would be an </p>
   </div>
   <div class="nft__quote-desktop">
-    <p>"if clarendon was released today, </p>
-    <p> it would be an nft"</p>
+    <p class="nft__actual-quote">"If clarendon was released today, </p>
+    <p class="nft__actual-quote"> it would be an nft"</p>
     <p class="quote__author">- Boeckaert M.</p>
   </div>
   <h3 class="nft__title">nft?!</h3>
-  <p class="nft__text">NFT stands for <span class="orange">Non-Fungible Token</span>. It is a digital art piece or a digital version of a physical artwork and is uploaded on the blockchain, a big excel file on the internet. It certifies a digital asset is therefore not interchangeable, it also gives image rights and ownership back to the creators.
-  </p>
+  <p class="nft__img">img</p>
+  <div class="nft__text-container">
+    <h2 class="nft__text-title">What is <br>an nft?</h2>
+    <p class="nft__text">NFT stands for <span class="orange">Non-Fungible Token</span>. It is a digital art piece or a digital version of a physical artwork and is uploaded on the blockchain, a big excel file on the internet. It certifies a digital asset is therefore not interchangeable, it also gives image rights and ownership back to the creators. But keep in mind that NFT's aren't that environmental friendly.
+    </p>
+  </div>
 </section>
 <!-- why an NFT section -->
 <section class="section__why">
@@ -165,7 +170,7 @@
 
 <article class="slanted">
   <div class="slanted__content">
-    <h4 class="slanted__title">Next series in Slanted:</h4>
+    <h4 class="slanted__title">Next week in Slanted:</h4>
     <p class="uppercase">Futura</p>
   </div>
 </article>
