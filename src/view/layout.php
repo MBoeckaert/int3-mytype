070a5fa91451f724efb57e4a58a41df88da84469
Matthias <matthiasboeckaert@gmail.com>
2022-01-28 11:07:51 +0100

    testing gsap

--- src/view/layout.php
+++ src/view/layout.php
@@ -13,6 +13,9 @@
   <div class="container">
     <?php echo $content;?>
   </div>
+
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.9.1/gsap.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.9.1/ScrollTrigger.min.js"></script>
   <?php echo $js; ?>
 </body>
 </html>
