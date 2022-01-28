7ad3067f712252b1e8bdbe789f8e6614c5c8bd2f
Matthias <matthiasboeckaert@gmail.com>
2022-01-28 10:35:52 +0100

    moved assets folder so it can be seen on heroku

--- src/js/script.js
+++ src/js/script.js
@@ -1,4 +1,4 @@
 export const init = () => {
   console.log('start executing this JavaScript');
- 
+  console.log('i am working');
 };
--- src/view/pages/index.php
+++ src/view/pages/index.php
@@ -3,16 +3,16 @@
   <div class="landing__img">
     <picture>
       <source media="(min-width: 1025px)"
-              srcset="../assets/img/clarendonDesktop.png"
+              srcset="assets/img/clarendonDesktop.png"
               sizes="40vw">
       <source media="(min-width: 767px)"
-              srcset="../assets/img/clarendonTablet.png">
-      <img class="img__billboard" src="../assets/img/clarendonMobile.png" alt="clarendon billboard" >
+              srcset="assets/img/clarendonTablet.png">
+      <img class="img__billboard" src="assets/img/clarendonMobile.png" alt="clarendon billboard" >
     </picture>
     <picture>
-        <source media="(min-width: 1025px)" srcset="../assets/img/typefaceDesktop.png">
-      <source media="(min-width: 767px)" srcset="../assets/img/typefaceTablet.png">
-      <img class="img__billboard" src="../assets/img/typefaceMobile.png" alt="typeface billboard" >
+        <source media="(min-width: 1025px)" srcset="assets/img/typefaceDesktop.png">
+      <source media="(min-width: 767px)" srcset="assets/img/typefaceTablet.png">
+      <img class="img__billboard" src="assets/img/typefaceMobile.png" alt="typeface billboard" >
     </picture>
   </div>
 
@@ -38,7 +38,7 @@
       <source media="(min-width: 1025px)"
               srcset="../assets/img/capitalR.png"
               sizes="40vw">
-      <img class="img__capital-r" src="../assets/img/capitalR.png" alt="capital letter R" >
+      <img class="img__capital-r" src="assets/img/capitalR.png" alt="capital letter R" >
     </picture>
   </div>
   <div class="creator__title">
@@ -118,10 +118,10 @@
     <div class="picture__container">
       <picture>
         <source media="(min-width: 1025px)"
-                srcset="../assets/img/clarendonNFT.png"
+                srcset="assets/img/clarendonNFT.png"
                 sizes="40vw">
-        <source media="(min-width: 767px)" srcset="../assets/img/clarendonNFTmobile.png">
-        <img class="reasons__img" src="../assets/img/clarendonNFTmobile.png" alt="clarendon billboard" > <!--  do i need width & height here? -->
+        <source media="(min-width: 767px)" srcset="assets/img/clarendonNFTmobile.png">
+        <img class="reasons__img" src="assets/img/clarendonNFTmobile.png" alt="clarendon billboard" > <!--  do i need width & height here? -->
       </picture>
     </div>
     <!-- <p class="reasons__img">Here comes an img</p> -->
